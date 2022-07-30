be6c9959adfc28888e9b7e39c6c842ebc805d6e9
JamieCBarr <jamiecbarr@gmail.com>
2022-07-30 15:58:20 -0400

    created userAccount controller

--- application/src/main/java/org/wecancodeit/serverside/repository/UserRepo.java
+++ application/src/main/java/org/wecancodeit/serverside/repository/UserRepo.java
@@ -3,5 +3,7 @@ package org.wecancodeit.serverside.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.wecancodeit.serverside.model.User;
 
+import java.util.Optional;
+
 public interface UserRepo extends CrudRepository <User, Long>{
-}
+    Optional<User> findByUsername(String username);
