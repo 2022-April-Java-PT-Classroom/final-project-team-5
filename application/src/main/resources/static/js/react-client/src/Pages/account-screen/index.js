24309fa85632e9eafebce4252190b5a9dd34cc77
JamieCBarr <jamiecbarr@gmail.com>
2022-07-31 15:20:05 -0400

    basic profile page that displays username

--- application/src/main/resources/static/js/react-client/src/Pages/account-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/account-screen/index.js
@@ -1,8 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import Axios from 'axios';
+
 import style from './style.module.scss';
 
 const AccountScreen=()=>{
+  const [loadingUser, setLoadingUser] = useState(true);
+  const [user, setUser] = useState(null);
+
+  let {userName} = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await Axios(`http://localhost:8080/users/{username}`);
+      setUser(result.data);
+    }
+    if(user){
+      setLoadingUser(false);
+    }
+    const timer = setTimeout(() =>{
+      !user && fetchData();
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [user]);
+
   return(
-    <h2>This is Account Screen</h2>
+    <body className={style.body}>
+      {loadingUser ? <h3>Finding Profile Info...</h3> : 
+        <div className={style.userProfile}>
+          <h3 className={style.username}>{user.username}</h3>
+        </div>
+      }
+    </body>
   )  
 }
 export default AccountScreen;
