68b68de87e3e121b2052fe0cbc50edb6c0a44b04
grullonvictor06 <grullonvictor06@gmail.com>
2022-07-31 00:38:22 -0400

    everything linked to home page

--- application/src/main/resources/static/js/react-client/src/Pages/home-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/home-screen/index.js
@@ -1,4 +1,7 @@
+import Event from '../../Components/event';
 import Login from '../../Components/login';
+import News from '../../Components/news-article';
+import Post from '../../Components/post';
 import style from './style.module.scss';
 
 const HomeScreen=()=>{
@@ -7,6 +10,9 @@ const HomeScreen=()=>{
         <div className={style.HomeScreen}>
             <Login/>
             <h2>This is the HomeScreen</h2>
+            <News/>
+            <Event/>
+            <Post/>
         </div>
     )
 }
