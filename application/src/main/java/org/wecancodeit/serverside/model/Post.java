22a6ce29a53427aca8a93f11d5cfb105cad4b641
scald97 <serena.ann.caldarella@gmail.com>
2022-07-31 00:52:22 -0400

    first post

--- application/src/main/java/org/wecancodeit/serverside/model/Post.java
+++ application/src/main/java/org/wecancodeit/serverside/model/Post.java
@@ -1,4 +1,38 @@
 package org.wecancodeit.serverside.model;
 
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
 public class Post {
+
+    @Id@GeneratedValue
+
+    public Long id;
+
+    public String post;
+
+    @ManyToOne
+    public Collection <User> users;
+
+    @ManyToMany
+    public Collection<Comment> comments;
+
+    @OneToMany
+    public Collection<Topic> topics;
+    public Post(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public Post(String post) {
+        this.post = post;
+    }
 }
+
