0b3a455a57836224cd7ce0ae12bdcd5cc757d7ea
grullonvictor06 <grullonvictor06@gmail.com>
2022-07-31 02:14:14 -0400

    logo solved just need resized

--- application/src/main/resources/static/js/react-client/src/Components/header/index.js
+++ application/src/main/resources/static/js/react-client/src/Components/header/index.js
@@ -4,7 +4,7 @@ import style from './style.module.scss';
 const Header=()=>{
     return(
         <div>
-            <img src="https://i.imgur.com/HQSTt1W" alt="AgapeLogo.gif"></img>
+            <img src="https://i.imgur.com/HQSTt1W.gif" alt="AgapeLogo.gif"></img>
             <h2>Agape</h2>
             <Login/>
         </div>
