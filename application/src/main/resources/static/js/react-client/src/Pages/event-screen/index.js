7cc5312c2ab36ea7849faee03a1fb2ec27152947
acolter12 <acolter@kent.edu>
2022-08-01 20:28:33 -0400

    Started event screen index.js

--- application/src/main/resources/static/js/react-client/src/Pages/event-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/event-screen/index.js
@@ -1,6 +1,29 @@
+import { useEffect, useState } from 'react';
+
 import style from './style.module.scss';
 
 const EventScreen=()=>{
+
+  const [events, setEvents] = useState(null),
+        [loadingEvents, setLoadingEvents] = useState(true);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const result = await Axios('http://localhost:8080/api/events');
+      setEvents(result.data);
+    }
+    
+    if(events) {
+      setLoadingEvents(false);
+    }
+
+    const timer = setTimeout(() => {
+      !events && fetchEvents();
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [events]);  
+
+  
   return(
     <h2>This is the Event Screen</h2>
   )  
