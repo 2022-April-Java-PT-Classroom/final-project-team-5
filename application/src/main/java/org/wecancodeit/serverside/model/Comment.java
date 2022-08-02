2de81bd9edd227e52d3f01be3c34c8fcc825377a
ackley787 <ackley787@gmail.com>
2022-08-01 21:31:22 -0400

    somethings wrong with intellij not showing missint imports and underlining code

--- application/src/main/java/org/wecancodeit/serverside/model/Comment.java
+++ application/src/main/java/org/wecancodeit/serverside/model/Comment.java
@@ -1,4 +1,28 @@
 package org.wecancodeit.serverside.model;
 
+@Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue
+    private String commentName;
+
+    @ManyToMany
+    private Collection<Comment> comments;
+
+    @ManyToMany
+    private Collection<Post> posts;
+
+    @ManyToMany
+    private Collection<Event> events;
+
+    public Comment {}
+
+    public Comment (String commentName) {
+
+        this.commentName = commentName;
+    }
+
+
+
 }
