77ba577071db6c9b0aff07cf4c0bad8d42ee4211
ackley787 <ackley787@gmail.com>
2022-08-06 15:21:24 -0400

    added pic and bio and some styling for about screen page

--- application/src/main/resources/static/js/react-client/src/Pages/about-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/about-screen/index.js
@@ -14,47 +14,63 @@ const AboutScreen = () => {
         <div className={style.aboutScreen}>
             
                 <div className={style.section1}>
-
-                <img className={style.img} src={Dhana} alt='PP' /> 
+                <section className={style.background}>
                 <h2>Dhana P Acharya</h2>
-                
+                <img className={style.img} src={Dhana} alt='PP' /> 
+                </section>              
                 <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
                      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
                      laboris nisi ut aliquip ex ea commodo consequat.
                      Duis aute irure dolor in reprehenderit</p>
-
-                <img className={style.img} src={Jamie} alt='PP' />
+                
+                <section className={style.background}>
                 <h2>Jamie Barr</h2>
-                
+                <img className={style.img} src={Jamie} alt='PP' />
+                </section>   
+                              
                 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                       incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
-                      
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+                
+                <section className={style.background}>
+                <h2>Adrian Colter</h2>      
                 <img className={style.img} src={Adrian} alt='PP' />
-                <h2>Adrian Colter</h2>
-                
+                </section>   
+                                
                 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-               incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-               ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit </p>
+                 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                 ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit </p>
 
                 </div>
 
                 <div className={style.section2}>
-                  
-                <img className={style.img} src={Victor} alt='PP' />
+                
+                <section className={style.background}>
                 <h2>Victor Grullon</h2>
+                <img className={style.img} src={Victor} alt='PP' />
+                </section>   
+                
                 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
                 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
                 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit</p>
-
-                <img className={style.img} src={Adam} alt='PP' />
+                
+                <section className={style.background}>
                 <h2>Adam Ackley</h2>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                   incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                   ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit </p>
+                <img className={style.img} src={Adam} alt='PP' />
+                </section>   
+               
+                <p>I am currently enrolled at We Can Code IT to earn a Certificate in Software 
+                    Development. It is my goal to become a Full-Stack Developer. I have a 
+                    passion for coding and enjoy working with others in a team setting as well 
+                    as independently. I am an efficient problem solver with excellent 
+                    troubleshooting skills. I am flexible and always seeking to acquire new skills. Besides having earned an Associates degree in Network Systems Administration. I am an avid computer gamer and have great interest in computer hardware and remote control cars.</p>
 
-                 <img className={style.img} src={Serena} alt='PP' />
-                 <h2>Serena Howard</h2>
+                <section className={style.background}>
+                <h2>Serena Howard</h2>
+                <img className={style.img} src={Serena} alt='PP' />
+                </section>   
+
+
                 <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
                      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
                     exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit</p>
