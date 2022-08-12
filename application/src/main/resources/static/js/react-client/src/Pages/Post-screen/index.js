7c966c475f7ff9ce42c5edd0ecbd042d20eed2d5
acolter12 <acolter@kent.edu>
2022-08-12 01:11:47 -0400

    Add a comment text is displaying, but comment is not display.

--- application/src/main/resources/static/js/react-client/src/Pages/Post-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/Post-screen/index.js
@@ -1,20 +1,26 @@
 import { useEffect, useState } from 'react';
 
 import AddPost  from '../../Components/post';
-import Axios from 'axios';
+import  Axios  from 'axios';
+import axios from 'axios';
 import style from './style.module.scss';
+import { useParams } from 'react-router-dom';
 
 const PostScreen=()=>{
 
+    const { id } = useParams();
     const [posts, setPost] = useState(null),
-        [loadingPost, setLoadingPost] = useState(true);
+          [loadingPost, setLoadingPost] = useState(true);
+    const [commentState, setCommentState] = useState({
+          commentContent: ""
+        });
 
 
   useEffect(() => {
     const fetchPost = async () => {
-      const result = await Axios('http://localhost:8080/api/post');
+      const result = await Axios(`http://localhost:8080/api/post`);
+      console.log(result.data);
       setPost(result.data);
-      console.log(result.data)
     }
     
     if(posts) {
@@ -26,9 +32,35 @@ const PostScreen=()=>{
     }, 1000);
     return () => clearTimeout(timer);
   }, [posts]);  
+  
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setCommentState({
+    ...commentState,
+    [event.target.name] : value
+});
+}
+
+const handleSubmit = (event) => {
+
+  const userData = {
+    commentContent: commentState.commentContent
+}
+
+axios.post(`http://localhost:8080/api/post/${id}/add-comment`, userData).
+then((response) => {
+    console.log(response.status);
+    console.log(response.data);
+    setPost(response.data);
+})
+}
 
   
 return (
+
+  loadingPost ? <h2>Loading</h2> :
+
     <div>
         <section className={style.postForum}>
         <h2>Create A Post</h2>
@@ -41,11 +73,29 @@ return (
               {posts.map(post => (
                 <div key={post.id}>
                     <p className={style.postBox}>{post.bodyOfPost}</p>
+                    
                 </div>
               ))}  
             </div>
 }
+{posts.comments && posts.comments.map(comment=> (
+  <div key={comment.id}>
+                      <p>{comment.commentContent}</p>
+                      </div>
+                  ))}
+                 
+              <form onSubmit={handleSubmit}>
+                <input type="text"
+                name="commentContent"
+                value={commentState.commentContent}
+                onChange={handleChange}
+                placeholder='Write'
+            />
+            <button type='submit'>Add Comment</button>
+            </form>
+  
     </div>
+    
     // <div>
     //     <section className={style.postForm}>
     //         <h2 className={style.posts}></h2>
@@ -57,5 +107,6 @@ return (
     // </div> 
 );
  }  
+
 
 export default PostScreen
