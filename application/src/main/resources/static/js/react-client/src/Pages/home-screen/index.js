98f80344234149c496db6f9e33cf98928c495c93
scald97 <serena.ann.caldarella@gmail.com>
2022-08-13 19:56:26 -0400

    homepage styling

--- application/src/main/resources/static/js/react-client/src/Pages/home-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/home-screen/index.js
@@ -1,31 +1,24 @@
-import Event from '../../Components/events';
-import Login from '../../Components/login';
-import NewsScreen from '../../Pages/news-screen';
-import Post from '../../Components/post';
+
 import style from './style.module.scss';
+import React from 'react';
 
 const HomeScreen=()=>{
     return(
         <main>
-        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"/>
-          <div className={style.mainScreen}>
-            <div className={style.sidebar}>
-                 <a href="/" className={style.navListItem}><i class="uil uil-home"></i><h3>Home</h3></a>
-                 <a href="/event" className={style.navListItem}><i class="uil uil-calender"></i><h3>Events</h3></a> 
-                 <a href="/protest" className={style.navListItem}><i class="uil uil-megaphone"></i><h3>Learn how to protest</h3></a>
-                 <a href="/faq-screen" className={style.navListItem}><i class="uil uil-question-circle"></i><h3>FAQ</h3></a>
-                 <a href="/email-us" className={style.navListItem}><i class="uil uil-envelope"></i><h3>Email-Us</h3></a>
-                 <a href="/about-screen" className={style.navListItem}><i class="uil uil-smile"></i><h3>Meet the team</h3></a>
-
+          {/* <div className={style.slides}>
+            <div className={style.slide}>
+                
             </div>
+
+          </div> */}
             <div className={style.post}>
-            <a href="/lgbtq" className={style.buttons}><h2>LGBTQ Rights</h2></a>
-            <a href="/racial" className={style.buttons}><h2>Cultural Rights</h2></a>
-            <a href="/women" className={style.buttons}><h2>Women's Rights </h2></a>
+                 <a href="/lgbtq" className={style.buttons}><h2 className={style.title}>LGBTQ Rights</h2></a>
+                 <a href="/racial" className={style.buttons}><h2 className={style.title}>Cultural Rights</h2></a>
+                 <a href="/women" className={style.buttons}><h2 className={style.title}>Women's Rights </h2></a>
               
             </div>
-         </div>
+    
         </main>
     )
 }
-export default HomeScreen;
+export default HomeScreen
