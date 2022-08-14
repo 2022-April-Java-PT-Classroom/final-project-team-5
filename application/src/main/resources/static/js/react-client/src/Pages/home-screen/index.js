048a3d7aa635839b7914fdcb9ec2ec775274e348
scald97 <serena.ann.caldarella@gmail.com>
2022-08-14 15:54:56 -0400

    new photos in carrousel

--- application/src/main/resources/static/js/react-client/src/Pages/home-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/home-screen/index.js
@@ -1,11 +1,11 @@
 import protest from"../../Assets/Images/clay-banks-OvGIFsexb-8-unsplash.jpg"
 import GhandiPhoto from "../../Assets/Images/GhandiQoute.png"
 import mlk from "../../Assets/Images/mlkqoute.png"
-import myBody from "../../Assets/Images/myBody.jpg";
+import myBody3 from "../../Assets/Images/myBody3.jpg";
 import lgbtq from '../../Assets/Images/lgbtq.jpg';
 import endRacism from "../../Assets/Images/endRacism.jpg";
 import rosaParks from "../../Assets/Images/rosaParks.png";
-import silence from "../../Assets/Images/silence.jpg";
+import humanity from "../../Assets/Images/humanity.jpg";
 import lgbtq2 from "../../Assets/Images/lgbtq2.jpg"
 
 import style from './style.module.scss';
@@ -19,13 +19,13 @@ const HomeScreen=()=>{
         <div className={style.contentCarrousel}>
           <figure className={style.shadow}><img src={protest}/></figure>
           <figure className={style.shadow}><img src={GhandiPhoto}/></figure>
-          <figure className={style.shadow}><img src={myBody}/></figure>
+          <figure className={style.shadow}><img src={myBody3}/></figure>
           <figure className={style.shadow}><img src={lgbtq}/></figure>
           <figure className={style.shadow}><img src={mlk}/></figure>
-          <figure className={style.shadow}><img src={endRacism}/></figure>
+          <figure className={style.shadow}><img src={humanity}/></figure>
           <figure className={style.shadow}><img src={lgbtq2}/></figure>
           <figure className={style.shadow}><img src={rosaParks}/></figure>
-          <figure className={style.shadow}><img src={silence}/></figure>
+          <figure className={style.shadow}><img src={endRacism}/></figure>
     </div>
     <h2 className={style.mission1}>Our Mission:</h2>    
     <h2 className={style.mission2}>Agape exist to band people together to fight against the injustices we are witnessing everyday in a safe and educated manner.</h2>
