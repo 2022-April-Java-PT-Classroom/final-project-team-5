0370abe8c93147244ad6a8e9d76254f28c0ba952
scald97 <serena.ann.caldarella@gmail.com>
2022-08-14 01:13:24 -0400

    homepage styling almost done

--- application/src/main/resources/static/js/react-client/src/Pages/home-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/home-screen/index.js
@@ -1,17 +1,37 @@
-import slide from "../../Assets/Images/silence.jpg"
+import protest from"../../Assets/Images/clay-banks-OvGIFsexb-8-unsplash.jpg"
+import GhandiPhoto from "../../Assets/Images/GhandiQoute.png"
+import mlk from "../../Assets/Images/mlkqoute.png"
+import myBody from "../../Assets/Images/myBody.jpg";
+import lgbtq from '../../Assets/Images/lgbtq.jpg';
+import endRacism from "../../Assets/Images/endRacism.jpg";
+import rosaParks from "../../Assets/Images/rosaParks.png";
+import silence from "../../Assets/Images/silence.jpg";
+import lgbtq2 from "../../Assets/Images/lgbtq2.jpg"
+
 import style from './style.module.scss';
 import React from 'react';
 
 const HomeScreen=()=>{
     return(
         <main>
-            <h2>hi</h2>
-          {/* <div className={style.slides}>
-            <div className={style.slide}>
-            <img className={style.slide-image} alt='' src ={slide}></img>
-           </div>
-                
-          </div> */}
+<section className={style.slideshow}>
+      <div className={style.entireContent}>
+        <div className={style.contentCarrousel}>
+          <figure className={style.shadow}><img src={protest}/></figure>
+          <figure className={style.shadow}><img src={GhandiPhoto}/></figure>
+          <figure className={style.shadow}><img src={myBody}/></figure>
+          <figure className={style.shadow}><img src={lgbtq}/></figure>
+          <figure className={style.shadow}><img src={mlk}/></figure>
+          <figure className={style.shadow}><img src={endRacism}/></figure>
+          <figure className={style.shadow}><img src={lgbtq2}/></figure>
+          <figure className={style.shadow}><img src={rosaParks}/></figure>
+          <figure className={style.shadow}><img src={silence}/></figure>
+    </div>
+    <h2 className={style.mission1}>Our Mission:</h2>    
+    <h2 className={style.mission2}>Agape exist to band people together to fight against the injustices we are witnessing everyday in a safe and educated manner.</h2>
+  </div>
+</section>
+           
             <div className={style.post}>
                  <a href="/lgbtq" className={style.buttons}><h2 className={style.title}>LGBTQ Rights</h2></a>
                  <a href="/racial" className={style.buttons}><h2 className={style.title}>Cultural Rights</h2></a>
