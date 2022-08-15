53c2378ea278db0f34d80fefa97b0fa136c81a8d
JamieCBarr <jamiecbarr@gmail.com>
2022-08-15 12:06:32 -0400

    added icons and phone/media sections

--- application/src/main/resources/static/js/react-client/src/Pages/email-us/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/email-us/index.js
@@ -3,10 +3,11 @@ import style from './style.module.scss';
 
 const Email = () => {
     return (
+      <div className={style.container}>
         <div className={style.emailus}>
             <form action="email-us">
                 <div className={style.email}>
-                    
+                        <h5><i>&#9993;</i> E-mail us!</h5>
                         <input placeholder=" Email Address" type="email" required />
                      <div>
                         <textarea placeholder=" Message here" rows="10" cols="50" required></textarea>
@@ -17,6 +18,21 @@ const Email = () => {
                 </div>
             </form>
         </div>
+
+        <div className={style.callus}>
+            <h5><i>&#9990;</i> Call us!</h5>
+            <button>(216) 555-0142</button>
+
+        </div>
+        
+        <div className={style.media}>
+            <h5><i>&#8886;</i> Connect with us!</h5>
+            <button className={style.mediaButton}>&#128330;</button>
+            <button className={style.mediaButton}>f</button>
+            <button className={style.mediaButton}><p>&#9655;</p></button>
+            <button className={style.mediaButton}>&#10695;</button>
+        </div>
+      </div>
     );
 }
 export default Email;
