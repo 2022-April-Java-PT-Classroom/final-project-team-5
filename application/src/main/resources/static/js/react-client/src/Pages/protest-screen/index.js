e902ba922b2e74e04695f6a2e48d8231235e9363
scald97 <serena.ann.caldarella@gmail.com>
2022-08-18 22:11:40 -0400

    resource page done organized organizations

--- application/src/main/resources/static/js/react-client/src/Pages/protest-screen/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/protest-screen/index.js
@@ -12,16 +12,16 @@ const ProtestScreen=()=>{
         <section className={style.infoBody}>
             <article className={style.howTo}>
                 <p>Here are a few resources to help with the process!</p>
+                <a href="https://www.nonviolent-conflict.org/resource-library/?fwp_language=english" target='_blank'><button className={style.resources}>Non-Violent Conflict</button></a>
                 <a href="https://www.activisthandbook.org/en/organising/protest" target='_blank'><button className={style.resources}>Activist Handbook</button></a>
                 <a href="https://www.amnestyusa.org/" target='_blank'><button className={style.resources}>Amnesty USA</button></a>
-                <a href="https://www.nonviolent-conflict.org/resource-library/?fwp_language=english" target='_blank'><button className={style.resources}>Non-Violent Conflict</button></a>
            
              </article>
 
             <article className= {style.rights}>
                  <p>Here is where you can learn your rights!</p>
-                  <a href="https://cldc.org/know-your-rights/" target='_blank'><button className={style.resources}>Civil Liberties Defense</button></a>
                   <a href="https://www.americanbar.org/news/abanews/publications/youraba/2018/november-2018/consider-this-advice-before-adding-civil-disobedience-defense-to/" target='_blank'><button className={style.resources}>American Bar</button></a>
+                  <a href="https://cldc.org/know-your-rights/" target='_blank'><button className={style.resources}>Civil Liberties Defense</button></a>
                   <a href="https://www.nlg.org/resources/" target='_blank'><button className={style.resources}>National Lawyers Guild</button></a>
             </article>
        </section>
@@ -31,22 +31,22 @@ const ProtestScreen=()=>{
       <div className={style.organizations}>
        <section className={style.lgbtqRights}>
           <h2>LGBTQ Organizations</h2>
-            <a href="https://www.thetaskforce.org/" target='_blank'><button className={style.resources2}>National LGBTQ Taskforce</button></a>
             <a href="https://www.lambdalegal.org/" target='_blank'><button className={style.resources2}>Lambda Legal for Assistance</button></a>
+            <a href="https://www.thetaskforce.org/" target='_blank'><button className={style.resources2}>National LGBTQ Taskforce</button></a>
             <a href="https://www.hrc.org/" target='_blank'><button className={style.resources2}>Human Rights Campaign</button></a>
       </section>
    
       <section className={style.culturalRights}>
          <h2>Rights of Minorities Organizations</h2>
-          <a href="https://colorofchange.org/" target='_blank'><button className={style.resources2}>Color of Change</button></a>
           <a href="https://civilrights.org/" target='_blank'><button className={style.resources2}> Rights of all Persons</button></a>
+          <a href="https://colorofchange.org/" target='_blank'><button className={style.resources2}>Color of Change</button></a>
           <a href="https://www.equalrights.org/" target='_blank'><button className={style.resources2}>Media Justice</button></a>
       </section>
 
        <section className={style.womensRights}>
           <h2>Womens Rights Organizations</h2>
-            <a href="https://feminist.org/" target='_blank'><button className={style.resources2}>Feminist Majority Foundation</button></a>
             <a href="https://now.org/" target='_blank'><button className={style.resources2}>National Organization for Women</button></a>
+            <a href="https://feminist.org/" target='_blank'><button className={style.resources2}>Feminist Majority Foundation</button></a>
             <a href="https://www.equalrights.org/" target='_blank'><button className={style.resources2}>Equal Rights Advocates</button></a>
        </section> 
    
