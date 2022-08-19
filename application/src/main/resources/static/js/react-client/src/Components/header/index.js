fb89ba3352d1b33103b9458481387ffc0a572842
scald97 <serena.ann.caldarella@gmail.com>
2022-08-18 21:18:22 -0400

    took out hyphen

--- application/src/main/resources/static/js/react-client/src/Components/header/index.js
+++ application/src/main/resources/static/js/react-client/src/Components/header/index.js
@@ -11,7 +11,7 @@ const Header=()=>{
                         <a href="/protest"><h2 className={style.each}>Resources</h2></a>
                         <a href="/forum"><h2 className={style.each}>Forum</h2></a>
                         <a href="/faq-screen"><h2 className={style.each}>FAQ</h2></a>
-                        <a href="/contact-us"><h2 className={style.each}>Contact-Us</h2></a>
+                        <a href="/contact-us"><h2 className={style.each}>Contact Us</h2></a>
                         <a href="/about-screen"><h2 className={style.each}>About Us</h2></a>
                        </div>
                 </nav>
