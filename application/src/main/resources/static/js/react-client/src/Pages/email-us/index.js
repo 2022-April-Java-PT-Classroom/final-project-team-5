90a4aacb9032c1da102ea67477df0586d32ee961
JamieCBarr <jamiecbarr@gmail.com>
2022-08-18 23:51:03 -0400

    larger contact page

--- application/src/main/resources/static/js/react-client/src/Pages/email-us/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/email-us/index.js
@@ -5,7 +5,7 @@ const Email = () => {
     return (
       <div className={style.container}>
         <div className={style.callus}>
-            <h2><i>&#9990;</i> Call</h2>
+            <h2 className={style.contactTitle}><i>&#9990;</i> Call</h2>
             <button className={style.phoneButton}>(216) 555-0142</button>
             <p>M-F 8am-10pm ET</p>
         </div>
@@ -13,11 +13,9 @@ const Email = () => {
         <div className={style.emailus}>
             <form action="email-us">
                 <div className={style.email}>
-                        <h2><i>&#9993;</i> E-mail</h2>
+                        <h2 className={style.contactTitle}><i>&#9993;</i> E-mail</h2>
                         <input placeholder=" Email Address" type="email" required />
-                     <div>
                         <textarea placeholder=" Message here" rows="10" cols="50" required></textarea>
-                    </div>
                 </div>
                 <div class="btn">
                     <button className={style.emailButton}  type='send'>Send</button>
@@ -27,7 +25,7 @@ const Email = () => {
 
         <div className={style.media}>
         <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"></link>
-            <h2><i>&#8886;</i> Connect</h2>
+            <h2 className={style.contactTitle}><i>&#8886;</i> Connect</h2>
             <div className={style.mediaRow}>  
               <button className={style.mediaButton}><p className={style.bird}><i class="uil uil-twitter"></i></p></button>
               <button className={style.mediaButton}><p className={style.book}><i class="uil uil-facebook-f"></i></p></button>
